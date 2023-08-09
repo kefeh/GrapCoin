949a6c3ccbac177bb539e2982cca692496329b51
kefeh <kefehcollins@gmail.com>
2023-08-09 10:20:53 +0100

    chore: updates color scheme of the app

--- lib/src/constants/colors.dart
+++ lib/src/constants/colors.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 
 const double maxWidth = 400.7;
 const double maxHeight = 766.1;
-const Color black = Color.fromARGB(255, 51, 51, 51);
+const Color blackish = Color.fromRGBO(51, 51, 51, 1);
+const Color black = Color(0xFF3E3E3E);
+
 const Color blackLight = Color.fromARGB(128, 51, 51, 51);
 const Color white = Color.fromRGBO(255, 255, 255, 1);
+const Color whitish = Color.fromARGB(255, 245, 245, 245);
 const Color whiteBlueLight = Color.fromARGB(255, 237, 245, 253);
 const Color blue = Color(0xff0B5CFF);
 const Color blueCyan = Color.fromARGB(255, 165, 234, 252);
 const Color blueCyanGreen = Color.fromARGB(255, 3, 255, 213);
-const Color red = Color.fromARGB(255, 255, 18, 18);
+const Color red = Color.fromARGB(255, 182, 25, 255);
 
 const Color darkBlue = Color(0xff1E1E1E);
 const Color lightBlue = Color.fromARGB(255, 228, 241, 255);
@@ -19,10 +22,11 @@ const Color grey = Color.fromARGB(255, 245, 245, 245);
 const Color lightWhite = Color.fromARGB(51, 255, 255, 255);
 const Color semiTransparentWhite = Color.fromARGB(125, 255, 255, 255);
 
-const Color purple = Color.fromARGB(255, 152, 104, 255);
-const Color purpleDeep = Color.fromARGB(255, 98, 25, 255);
-const Color purpleLight = Color.fromARGB(125, 152, 104, 255);
+const Color purpleDeep = Color.fromARGB(255, 82, 3, 253);
+const Color purple = Color.fromARGB(255, 98, 25, 255);
+const Color purpleLight = Color.fromARGB(255, 152, 106, 252);
 const Color purpleLighter = Color.fromARGB(124, 206, 185, 252);
+const Color purpleLighterer = Color.fromARGB(255, 244, 239, 253);
+const Color purpleLightest = Color.fromARGB(255, 251, 248, 255);
 
 const internalHosts = ['viewotesting.web.app', 'app.viewo.com'];
-const singleClassName = 'Coding Classes';
